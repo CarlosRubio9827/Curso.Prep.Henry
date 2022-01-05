079acd538841bc69fb2f392993214d713600dcf6
CarlosRubio9827 <rubiogallegoc@gmail.com>
2022-01-05 00:33:54 -0500

    Solución 04-JS-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,22 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0]
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1]
 }
+
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length
 }
 
 
@@ -23,13 +27,20 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (let i= 0; i<array.length;i++){
+    array[i] = array[i]+1 
+  }
+  return array;
 }
+
 
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array
 }
 
 
@@ -38,6 +49,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  return array
 }
 
 
@@ -47,6 +60,11 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var str = palabras[0]
+  for (let i= 1; i<palabras.length;i++){
+      str+=" "+palabras[i]
+  }
+  return str
 }
 
 
@@ -54,13 +72,26 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+
+  const x = array.find(i => {
+    
+      return i == elemento
+  }
+  );
+  return (x)? true: false
 }
+
 
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var sum= 0;
+  numeros.forEach(i =>{
+    sum+=i;
+  })
+  return sum
 }
 
 
@@ -68,13 +99,21 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest) / resultadosTest.length
+  
 }
-
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var mayor = numeros[0];
+  numeros.forEach(i=>{
+    if(i>mayor){
+      mayor= i
+    }
+  })
+  return mayor;
 }
 
 
@@ -82,13 +121,31 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var res = 1
+  if (arguments.length < 1){
+    return 0
+  }else if(arguments.length === 1){
+    return arguments[0]
+  }else{
+    for(let i= 0;i<arguments.length;i++){
+      res*= arguments[i];
+    }
+    return res
+  }
+  
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var x =0;
+  arreglo.forEach(i=>{
+    if (i >18){
+      x++
+    }
+  })
+  return x;
 }
 
 
@@ -97,22 +154,28 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  return(numeroDeDia ==1 || numeroDeDia == 7) ? "Es fin de semana" : "Es dia Laboral"
   
 } 
-
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  return (n.toString()[0] === '9')? true : false;  
 }
 
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var x = arreglo[0];
+  const y = arreglo.find(i=>{
+    return x != i
+  })
+
+  return (!y)? true: false;
   
 } 
 
@@ -122,6 +185,13 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  const x = []
+  array.forEach(i=>{
+    if(i=="Enero"||i=="Marzo"||i=="Noviembre"){
+      x.push(i);
+    }
+  })
+  return (x.length === 3) ? x : "No se encontraron los meses pedidos"
 }
 
 
@@ -129,6 +199,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var x = []
+  array.forEach(i=>{
+    if(i>100){
+      x.push(i)
+    }
+  })
+  return x
 }
 
 
@@ -140,8 +217,18 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let i= 1;
+  var x= []
+  while(i<=10){
+    numero+=2
+    x.push(numero)
+    if(i==numero){
+      break
+    }
+    i++
+  }
+  return(x.length == 10)? x : "Se interrumpió la ejecución"
 }
-
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -150,6 +237,19 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let i= 1;
+  var x= []
+  while(i<=10){
+    if(i == 5){
+      i++
+      continue
+    }else{
+      numero+=2
+      x.push(numero)
+    }
+    i++
+  }
+  return x
 }
 
 
